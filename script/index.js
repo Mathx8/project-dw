792f89c642ef56aa211b7c374d8aa4110e7c42ca
Mathx8 <matheusqueirozdev@gmail.com>
2024-09-21 12:51:26 -0300

    falta ajeitar sobre e footer

---
+++ script/index.js
@@ -0,0 +1,6 @@
+function AlertaLogin() {
+    let alerta = document.getElementById('fundo');
+    if (alerta.style.display == "none" || alerta.style.display == "") {
+        alerta.style.display = "flex";
+    }
+}
