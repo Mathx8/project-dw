11b0266bca63a53d4df3ff213b2010700fd4020d
Mathx8 <matheusqueirozdev@gmail.com>
2024-10-30 12:12:36 -0300

    Listagem de endereço

--- script/home.js
+++ script/home.js
@@ -1,6 +1,8 @@
 let perfil = document.getElementById('perfil');
 let sobre = document.getElementById('sobre');
 let carros = document.getElementById('carros');
+const url = "https://go-wash-api.onrender.com/api/auth/address";
+let token = JSON.parse(localStorage.getItem('user'));
 
 function AbrirPerfil() {
     perfil.style.display = "flex";
@@ -20,8 +22,51 @@ function AbrirCarros() {
     sobre.style.display = "none";
 }
 
-function ListUser() {
-    carros.style.display = "flex";
-    perfil.style.display = "none";
-    sobre.style.display = "none";
+async function ListarEndereco() {
+    try {
+        let api = await fetch(url, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token.access_token
+            }
+        });
+
+        if (!api.ok) {
+            throw new Error("Erro ao buscar endereços: " + api.statusText);
+        }
+
+        let resposta = await api.json();
+        console.log(resposta); 
+
+        let enderecos = resposta.data;
+
+        let conteudo = `
+        <h3>Seus Endereços</h3>
+        <table>
+            <tr>
+                <th>Título</th>
+                <th>Endereço</th>
+            </tr>`;
+
+        for (let dado of enderecos) {
+            conteudo += `
+                <tr>
+                    <td>${dado.title}</td>
+                    <td>${dado.formatted_address}</td>
+                </tr>`;
+        }
+
+        conteudo += `</table>`;
+
+
+        document.getElementById("lista-endereco").innerHTML = conteudo;
+        
+    } catch (error) {
+        console.error("Erro ao carregar endereços:", error);
+    }
+}
+
+window.onload = function () {
+    ListarEndereco();
 }
